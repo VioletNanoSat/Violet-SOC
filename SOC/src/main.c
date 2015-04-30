92361300043d6e700ce2fe5e7a9c4bd75d15af75
Gautam Ramaswamy <grr37@cornell.edu>
2015-04-30 19:47:54 -0400

    File for testing

--- SOC/src/main.c
+++ SOC/src/main.c
@@ -24,7 +24,7 @@
  * Include header files for all drivers that have been imported from
  * Atmel Software Framework (ASF).
  */
-#include <asf.h>
+
 #include <stdio.h>
 #include <stddef.h>
 #include <math.h>
@@ -52,17 +52,17 @@ double Result[/* SignalLen + KernelLen - 1 */])
 	}
 }
 
-void printSignal(const char* Name,
-double Signal[/* SignalLen */], size_t SignalLen)
-{
-	size_t i;
+// void printSignal(const char* Name,
+// double Signal[/* SignalLen */], size_t SignalLen)
+// {
+// 	size_t i;
 
-	for (i = 0; i < SignalLen; i++)
-	{
-		printf("%s[%zu] = %f\n", Name, i, Signal[i]);
-	}
-	printf("\n");
-}
+// 	for (i = 0; i < SignalLen; i++)
+// 	{
+// 		printf("%s[%zu] = %f\n", Name, i, Signal[i]);
+// 	}
+// 	printf("\n");
+// }
 #define ELEMENT_COUNT(X) (sizeof(X) / sizeof((X)[0]))
 
 
@@ -74,8 +74,7 @@ int step_function(int i){
 	return i >= 0;
 }
 
-void algorithm_1(double sampled_i[], double f[]){
-	int n=ELEMENT_COUNT(sampled_i);
+void algorithm_1(double sampled_i[], double f[], int n){
 	double f_norm[n];
 	double i_norm[n];
 	for(int i=0;i<n;i++){
@@ -91,8 +90,8 @@ void algorithm_1(double sampled_i[], double f[]){
 	}
 }
 
-void compute_uf(double f[], double uf[]){
-	int size=ELEMENT_COUNT(f);
+void compute_uf(double f[], double uf[],int size){
+	
 	double u[size];
 	for(int i=0;i<size;i++){
 		u[i]=step_function(i);
@@ -125,64 +124,86 @@ double compute_OCV(double vf[],double max, int max_index){
 	}*/
 }
 
-void compute_h(double OCV, double vf[], double uf[], double h[]){
-	int size=ELEMENT_COUNT(h);
-	for int(i=0;i<size;i++){
+void compute_h(double OCV, double vf[], double uf[], double h[], int size){
+	int i=0;
+	for (i=0;i<size;i++){
 		h[i]=vf[i]-OCV*uf[i];
 	}
 }
 
-void compute_new_voltage(double sampled_v[], double sampled_current[], )
+double compute_new_voltage(double sampled_v, double sampled_current, double h[], unsigned int sample_counter, double ixh){
+	double term=h[sample_counter]*sampled_current;
+	ixh=ixh+term;
+	return sampled_v-ixh;
+}
+
+void delay(unsigned int n){
+	int i=0;
+	for(i;i<n;i++);
+}
 
 
 int main (void)
 {
-	board_init();
-
 	// Insert application code here, after the board has been initialized.
-	int sample_counter=0;
-	double sampled_i[1000];
+	unsigned int sample_counter=0;
+	double sampled_i={.001};
 	double SOC;
-	double sampled_v[1000];
-	double f[1000];
-	double vf[1000];
-	double uf[1000];
+	double sampled_v={8};
+	double f[5];
+	double vf[5];
+	double uf[5];
 	int max_index=0;
 	double max;
 	double OCV;
-	double SOC_lookup_table[1200];
-	double current_lookup_table[1200];
-	double h[1000];
-	for (int i=0;i<1200;i++){
-		SOC_lookup_table[i]=(double) i/100;	
+	double SOC_lookup_table[12];
+	double current_lookup_table[10];
+	double h[5];
+	for (int i=0;i<12;i++){
+		SOC_lookup_table[i]=(double) i;	
 	}
-	for (int i=0;i<1200;i++){
+	for (int i=0;i<10;i++){
 		current_lookup_table[i]=(double) 0;
 	}
 	//Initial samples
-	while(sample_counter<1000){
-		sampled_i[sample_counter]=input_current;
-		sampled_v[sample_counter]=input_voltage;
-		sample_counter++;
-		delay(10);
-	}
+	// while(sample_counter<5){
+	// 	sampled_i[sample_counter]=input_current;
+	// 	sampled_v[sample_counter]=input_voltage;
+	// 	sample_counter++;
+	// 	delay(10);
+	// }
 	sample_counter=0;
 	//First measurement for SOC
-	algorithm_1(sampled_i,f);
+	int n=ELEMENT_COUNT(sampled_i);
+	algorithm_1(sampled_i,f,n);
 	compute_vf(f,sampled_v,vf);
-	compute_uf(f,uf);
+	int size=ELEMENT_COUNT(f);
+	compute_uf(f,uf,size);
 	max=max_uf(uf,max_index);
 	OCV=compute_OCV(vf,max,max_index);
-	compute_h(OCV,vf,uf,h);
-	SOC_index=round(OCV*100);
+	n=ELEMENT_COUNT(h);
+	compute_h(OCV,vf,uf,h,n);
+	int SOC_index=round(OCV*100);
 	SOC=SOC_lookup_table[SOC_index];
-	coulomb_count=current_lookup_table[round(SOC*100)];
+	int rounded_index=round(SOC*100);
+	double coulomb_count=current_lookup_table[rounded_index];
 	while(1){
-		algorithm_1(sampled_i,f);
+		double ixh=0;
+		sample_counter=0;
+		// while(sample_counter<1000){
+		// 	sampled_i[sample_counter]=input_current;
+		// 	sampled_v[sample_counter]=input_voltage;
+		// 	sampled_v[sample_counter]=compute_new_voltage(sampled_v[sample_counter],sampled_i[sample_counter],h,sample_counter,ixh);
+		// 	sample_counter++;
+		// 	delay(10);
+		// }
+		n=ELEMENT_COUNT(sampled_i);
+		algorithm_1(sampled_i,f,n);
 		compute_vf(f,sampled_v,vf);
-		compute_uf(f,uf);
+		size=ELEMENT_COUNT(f);
+		compute_uf(f,uf,size);
 		max=max_uf(uf,max_index);
-		OCV=compute_OCV(vf,max,max_index);	
+		OCV=compute_OCV(vf,max,max_index);
 	}
 	return 0;
 }
